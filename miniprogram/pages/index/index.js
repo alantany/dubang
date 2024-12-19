e6c6bc57c9f76dc5b9b725b481c3fe3f96154dc7
huaiyuantan <alantany@gmail.com>
2024-12-19 19:18:39 +0800

    修正图片路径：使用正确的相对路径

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -2,23 +2,23 @@
 Page({
   data: {
     buttonText: '点击开始录音',
-    currentImage: 'images/display/compressed/tea.jpg',
+    currentImage: '../../images/display/compressed/tea.jpg',
     description: '欢迎使用都邦健康，请点击按钮开始语音交互',
     serviceConfig: {
       '椅子': {
-        image: 'images/display/compressed/chair.jpg',
+        image: '../../images/display/compressed/chair.jpg',
         description: '人体工学椅，让您的腰部更舒适'
       },
       '血压': {
-        image: 'images/display/compressed/blood_pressure.jpg',
+        image: '../../images/display/compressed/blood_pressure.jpg',
         description: '智能血压计，随时监测您的健康'
       },
       '旅游': {
-        image: 'images/display/compressed/travel.jpg',
+        image: '../../images/display/compressed/travel.jpg',
         description: '特惠旅游套餐，放松身心'
       },
       '茶': {
-        image: 'images/display/compressed/tea.jpg',
+        image: '../../images/display/compressed/tea.jpg',
         description: '养生茶饮，调理身体'
       }
     }
