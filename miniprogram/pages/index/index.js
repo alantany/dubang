2a1b1611df9258da2d53e6051fe8051057e7997b
huaiyuantan <alantany@gmail.com>
2024-12-20 15:07:12 +0800

    修改：恢复使用video组件，更新视频加载逻辑

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -41,11 +41,35 @@ Page({
 
   loadVideo(videoId) {
     console.log('加载视频:', videoId)
-    // 构建实际的播放地址
-    const playUrl = `https://v.qq.com/txp/iframe/player.html?vid=${videoId}`
-    this.setData({
-      currentVideo: playUrl,
-      videoId: videoId
+    // 获取视频信息
+    wx.request({
+      url: `https://vv.video.qq.com/getinfo?vids=${videoId}&platform=101001&charge=0&otype=json`,
+      success: (res) => {
+        // 解析返回的数据
+        const dataStr = res.data.replace(/QZOutputJson=/, '')
+        const videoInfo = JSON.parse(dataStr)
+        if (videoInfo && videoInfo.vl && videoInfo.vl.vi && videoInfo.vl.vi[0]) {
+          const videoData = videoInfo.vl.vi[0]
+          const url = `http://ugcws.video.gtimg.com/${videoData.fn}?vkey=${videoData.fvkey}`
+          this.setData({
+            currentVideo: url,
+            videoId: videoId
+          })
+        } else {
+          console.error('获取视频信息失败')
+          wx.showToast({
+            title: '获取视频失败',
+            icon: 'error'
+          })
+        }
+      },
+      fail: (err) => {
+        console.error('请求视频信息失败:', err)
+        wx.showToast({
+          title: '获取视频失败',
+          icon: 'error'
+        })
+      }
     })
   },
 
